3a6dfe192aa0a0a29971ceb0b83eae8397ae62b9
Tatiana Marie Angèle <angeletatiana08@gmail.com>
2025-04-02 20:39:19 +0300

    fix: problème encodage

--- src/java/com/commanderepas/jdbc/Jdbc.java
+++ src/java/com/commanderepas/jdbc/Jdbc.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class Jdbc 
 {
-    private static final String URL = "jdbc:mysql://localhost:3306/commanderepas";
+    private static final String URL = "jdbc:mysql://localhost:3306/commanderepas?useUnicode=true&characterEncoding=utf8";
     private static final String USER = "root";
     private static final String PASSWORD = "";
     private static Connection connection;
